4abdf3d7619e25e7dd7b594006d42a9ed216b767
benaz <benazoulaydev@gmail.com>
2020-06-11 14:55:14 +0300

    working deprecated server

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,122 +1,132 @@
 package com.example.myapplication
-
+import android.content.Context
+import android.net.ConnectivityManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
-import android.widget.ArrayAdapter
-import android.widget.ListView
+import android.widget.EditText
 import android.widget.TextView
-import java.net.URL
-import android.os.Build
-import androidx.annotation.RequiresApi
-import androidx.lifecycle.LiveData
-import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.coroutines.CoroutineScope
+import android.widget.Toast
+import androidx.lifecycle.lifecycleScope
+import com.example.myapplication.R
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
-import kotlin.collections.ArrayList
+import kotlinx.coroutines.withContext
+import org.json.JSONException
+import org.json.JSONObject
+import java.io.BufferedWriter
+import java.io.IOException
+import java.io.OutputStreamWriter
+import java.net.HttpURLConnection
+import java.net.URL
+import javax.net.ssl.HttpsURLConnection
+
 
 class MainActivity : AppCompatActivity() {
-    private var viewModelJob = Job()
-    private val uiScope = CoroutineScope(Dispatchers.Main + viewModelJob)
+
+    lateinit var tvIsConnected: TextView
+    lateinit var etTitle: EditText
+    lateinit var etUrl: EditText
+    lateinit var etTags: EditText
+    lateinit var tvResult: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // insert into the history list
-        val list = findViewById<ListView>(R.id.historyList)
+        tvIsConnected = findViewById<TextView>(R.id.tvIsConnected)
+        etTitle = findViewById<EditText>(R.id.etTitle)
+        etUrl = findViewById<EditText>(R.id.etUrl)
+        etTags = findViewById<EditText>(R.id.etTags)
+        tvResult = findViewById<TextView>(R.id.tvResult)
 
-        /*uiScope.launch {
-            val conncetHistory = getlist()
-            val adapter: ArrayAdapter<String>
-            if (conncetHistory != null) {
-                adapter =
-                    ArrayAdapter(
-                        this@MainActivity,
-                        android.R.layout.simple_list_item_1,
-                        conncetHistory.toList()
-                    )
+        checkNetworkConnection()
+    }
+
+    public fun send(view:View) {
+        Toast.makeText(this, "Clicked", Toast.LENGTH_SHORT).show()
+        // clear text result
+        tvResult.setText("")
+
+        if (checkNetworkConnection())
+            lifecycleScope.launch {
+                val result = httpPost("http://10.0.2.2:65011/api/command")
+                tvResult.setText(result)
             }
-        }*/
+        else
+            Toast.makeText(this, "Not Connected!", Toast.LENGTH_SHORT).show()
+
     }
 
-    suspend fun getlist(): List<String> {
-        val conncetHistory = ListDatabase.getInsatnce(this).urlDatabase.getLastFive()
-        return conncetHistory
-    }
+    @Throws(IOException::class, JSONException::class)
+    private suspend fun httpPost(myUrl: String): String {
 
-    @RequiresApi(Build.VERSION_CODES.KITKAT)
-    fun tryConnect(view: View): Unit {
-        val url = findViewById<TextView>(R.id.urlinput).text.toString()
-        if (url == "") {
-            return
+        val result = withContext(Dispatchers.IO) {
+            val url = URL(myUrl)
+            // 1. create HttpURLConnection
+            val conn = url.openConnection() as HttpURLConnection
+            conn.requestMethod = "POST"
+            conn.setRequestProperty("Content-Type", "application/json; charset=utf-8")
+
+            // 2. build JSON object
+            //TODO change to real value depending on the joystick
+            val jsonObject = buidJsonObject(1.0, 1.0,1.0,1.0)
+            // 3. add JSON content to POST request body
+            setPostRequestContent(conn, jsonObject)
+
+            // 4. make POST request to the given URL
+            conn.connect()
+
+            // 5. return response message
+            conn.responseMessage + ""
         }
-
-        val apiURL = URL("https://localhost:44354/api/Connect")
-
-        var urlObj = UrlAddressList()
-        urlObj.url = url
-        val conncetHistory = ListDatabase.getInsatnce(this).urlDatabase.insert(urlObj)
-
-        finish()
-        startActivity(intent)
+        return result
     }
 
-/*
-    val connection = apiURL.openConnection() as HttpsURLConnection
-    connection.requestMethod = "POST"
-    connection.doOutput = true
+    private fun checkNetworkConnection(): Boolean {
+        val connMgr = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
 
-    val postData: ByteArray = url.toByteArray(StandardCharsets.UTF_8)
-
-    connection.setRequestProperty("charset", "utf-8")
-    connection.setRequestProperty("Content-length", postData.size.toString())
-    connection.setRequestProperty("Content-Type", "application/json")
-
-    try {
-        val stream = connection.outputStream
-        val outputStream: DataOutputStream = DataOutputStream(stream)
-        outputStream.write(postData)
-        outputStream.flush()
-    } catch (exeception: Exception) {
-
-    }
- */
-
-
-    /*with(apiURL.openConnection() as HttpsURLConnection) { // https/**/
-        requestMethod = "POST"
-
-        val wr = OutputStreamWriter(outputStream);
-        wr.write(url);
-        wr.flush();
-
-        BufferedReader(InputStreamReader(inputStream)).use {
-            val response = StringBuffer()
-
-            var inputLine = it.readLine()
-            while (inputLine != null) {
-                response.append(inputLine)
-                inputLine = it.readLine()
-            }
-
-            if (response.toString() == "OK") {
-                addSucssesURL(url)
-                displayNavigationScreen()
-            } else {
-                // print alert massage
-            }
+        val networkInfo = connMgr.activeNetworkInfo
+        val isConnected: Boolean = if(networkInfo != null) networkInfo.isConnected() else false
+        if (networkInfo != null && isConnected) {
+            // show "Connected" & type of network "WIFI or MOBILE"
+            tvIsConnected.text = "Connected " + networkInfo.typeName
+            // change background color to red
+            tvIsConnected.setBackgroundColor(-0x8333da)
+        } else {
+            // show "Not Connected"
+            tvIsConnected.text = "Not Connected"
+            // change background color to green
+            tvIsConnected.setBackgroundColor(-0x10000)
         }
-    }*/
-
-    private fun displayNavigationScreen() {
-
+        return isConnected
     }
 
-    fun cleaFiled(view: View) {
-        val url = findViewById<TextView>(R.id.urlinput)
-        url.text = ""
+    @Throws(JSONException::class)
+    private fun buidJsonObject(aileron:Double, rudder:Double, elevator:Double, throttle:Double)
+            : JSONObject {
+
+        val jsonObject = JSONObject()
+        jsonObject.accumulate("aileron", aileron)
+        jsonObject.accumulate("rudder", rudder)
+        jsonObject.accumulate("elevator", elevator)
+        jsonObject.accumulate("throttle", throttle)
+
+        //jsonObject.accumulate("tags", etTags.getText().toString())
+
+        return jsonObject
+    }
+
+    @Throws(IOException::class)
+    private fun setPostRequestContent(conn: HttpURLConnection, jsonObject: JSONObject) {
+
+        val os = conn.outputStream
+        val writer = BufferedWriter(OutputStreamWriter(os, "UTF-8"))
+        writer.write(jsonObject.toString())
+        Log.i(MainActivity::class.java.toString(), jsonObject.toString())
+        writer.flush()
+        writer.close()
+        os.close()
     }
 }
