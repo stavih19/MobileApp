18fcc6a6d9ac738c767f5a35dff6970c43906549
Hagai Stavi <hagai.stavi@live.biu.ac.il>
2020-06-17 14:27:28 +0300

    Toast

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
 import android.view.View
 import android.widget.ArrayAdapter
 import android.widget.ListView
@@ -65,7 +66,9 @@ class MainActivity : AppCompatActivity() {
 
     // show to user message
     private fun failedToSend(message: String) {
-        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+        val toast = Toast.makeText(this, message, Toast.LENGTH_SHORT)
+        toast.setGravity(Gravity.TOP, 0, 0)
+        toast.show()
     }
 
     // try to get image from user
